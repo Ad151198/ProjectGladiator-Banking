238c74813526fa76c9bb0f47f548d9fa70977168
Ad151198 <ad151198@gmail.com>
2021-08-10 00:50:00 +0530

    Commit2

--- Banking/src/main/java/com/lcf/app/beans/LoginCredentials.java
+++ Banking/src/main/java/com/lcf/app/beans/LoginCredentials.java
@@ -4,9 +4,7 @@ public class LoginCredentials {
 	
 	private String loginPassword;
 	private int transactionPin;
-	private CustomerDetails customerDetail;
-	
-//	private long customerId;
-//	this.customerId = CustomerDetails.customerId;
+	private long customerId;
+
 
 }
