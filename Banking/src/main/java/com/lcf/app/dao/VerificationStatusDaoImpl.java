9ce33ea4d8e57b5b12dc207bbe889bb8ceccf2a4
muskansoni0501 <muskansoni0501@gmail.com>
2021-08-13 11:28:06 +0530

    Forced Commit

--- Banking/src/main/java/com/lcf/app/dao/VerificationStatusDaoImpl.java
+++ Banking/src/main/java/com/lcf/app/dao/VerificationStatusDaoImpl.java
@@ -1,38 +1,41 @@
-//package com.lcf.app.dao;
-//
-//import java.util.List;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//import javax.transaction.Transactional;
-//
-//import com.lcf.app.beans.VerificationStatus;
-//
-//public class VerificationStatusDaoImpl implements VerificationStatusDao{
-//	
-//	@PersistenceContext
-//	EntityManager em;
-//
-//	@Override
-//	@Transactional
-//	public boolean createReference(VerificationStatus reference) {
-//		em.persist(reference);
-//		
-//		return true;
-//	}
-//
-//	@Override
-//	public long getReferenceById(long id) {
-//		VerificationStatus reference = em.find(VerificationStatus.class, id);
-//		
-//		return reference.getCustomerId();
-//	}
-//
-//	@Override
-//	public List<VerificationStatus> displayAll() {
-//		List<VerificationStatus> referenceList = em.createQuery("SELECT obj FROM VerificationStatus obj").getResultList();
-//		
-//		return referenceList;
-//	}
-//
-//}
+package com.lcf.app.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.lcf.app.beans.VerificationStatus;
+
+@Repository("referenceDao")
+public class VerificationStatusDaoImpl implements VerificationStatusDao{
+	
+	@PersistenceContext
+	EntityManager em;
+
+	@Override
+	@Transactional
+	public boolean createReference(VerificationStatus reference) {
+		em.persist(reference);
+		
+		return true;
+	}
+
+	@Override
+	public long getReferenceById(long id) {
+		VerificationStatus reference = em.find(VerificationStatus.class, id);
+		
+		return reference.getCustomerId();
+	}
+
+	@Override
+	public List<VerificationStatus> displayAll() {
+		List<VerificationStatus> referenceList = em.createQuery("SELECT obj FROM VerificationStatus obj").getResultList();
+		
+		return referenceList;
+	}
+
+}
